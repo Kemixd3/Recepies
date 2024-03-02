5c1d741921432bbf9eca1bddfad915e7ed03e1fa
Nikolai <nikolai2002b@gmail.com>
2024-03-02 15:26:21 +0100

    Added local files

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import { Categories } from "./recipes/Categories";
+import Recipe from "./recipes/Recipe";
+import Recipes from "./recipes/RecipeList";
+import RecipesLayout from "./recipes/RecipesLayout";
+import RecipeForm from "./recipes/RecipeForm";
+import Login from "./security/Login";
+//import Logout from "./security/_Logout";
+import Layout from "./Layout";
+import Home from "./Home";
+import "./App.css";
+
+export default function App() {
+  //const auth = useAuth();
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories/" element={<Categories />} />
+        <Route path="/recipes" element={<RecipesLayout />}>
+          <Route path="test" element={<h1>Test</h1>} />
+          <Route path=":id" element={<Recipe />} />
+        </Route>
+        <Route path="*" element={<h2>Not Found</h2>} />
+        <Route path="/add" element={<RecipeForm />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/logout" element={<Logout />} /> */}
+      </Routes>
+    </Layout>
+  );
+}
